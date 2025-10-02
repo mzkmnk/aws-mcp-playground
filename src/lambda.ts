913ec16e45f203330e5d9a82415d60bc92aca950
mizuki <sma0729mex@gmail.com>
2025-10-02 23:05:07 +0900

    refactor: 不必要なコードとパッケージ (#5)

--- src/lambda.ts
+++
@@ -1,7 +0,0 @@
-import serverlessExpress from '@codegenie/serverless-express';
-import { createMcpApp } from './app';
-
-// Create the MCP app
-const { app } = createMcpApp();
-
-export const handler = serverlessExpress({ app })
